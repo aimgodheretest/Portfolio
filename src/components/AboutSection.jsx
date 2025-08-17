2953f9b555039d64a00d38be9207e9197e670727
Murli Kumar <aimgodhere@gmail.com>
2025-08-17 19:01:56 +0530

    added resume link for download

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/cv.pdf"
+                href="https://drive.google.com/file/d/1sGAuCecQVnG-pm7wdCZJirgY8cWdVzDh/view?usp=sharing"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
