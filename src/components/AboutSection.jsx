bd4cfe09986105b118101a85c44e29ac95a34e0d
Murli Kumar <aimgodhere@gmail.com>
2025-08-22 12:29:17 +0530

    updated cv link

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/12yikmMlXuDQUKVy6Afq02p7kqaUSNGp3/view?usp=sharing"
+                href="https://drive.google.com/file/d/1S_JZOQ-JqnFLq3-DwpWlV-3e1czFlMbb/view?usp=sharing"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
